0e0cbd2bfb8c4f575bd70d7e24cfca38b0c86d7d
gabriel <gabriel.gregorio1987@gmail.com>
2023-10-05 02:35:04 -0300

    Chancing the picutre-card to dynamic

--- src/app/components/picture-card/picture-card.component.ts
+++ src/app/components/picture-card/picture-card.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-picture-card',
   templateUrl: './picture-card.component.html',
   styleUrls: ['./picture-card.component.css']
 })
-export class PictureCardComponent {
 
+export class PictureCardComponent implements OnInit {
+
+  @Input()
+  bigPicture: string = "";
+
+  ngOnInit(): void {}
+  
 }
