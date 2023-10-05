39daf51edc0bba17b5455f56e48e61deb0aa8e67
gabriel <gabriel.gregorio1987@gmail.com>
2023-10-05 02:55:33 -0300

    Changing text-card to dynamic

--- src/app/components/text-card/text-card.component.ts
+++ src/app/components/text-card/text-card.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-text-card',
   templateUrl: './text-card.component.html',
   styleUrls: ['./text-card.component.css']
 })
-export class TextCardComponent {
+export class TextCardComponent implements OnInit {
+
+  @Input()
+  leftImage: string = "";
+  @Input()
+  textH1: string = "";
+  @Input()
+  textP: string = "";
+  @Input()
+  firstImageRight: string = ""
+  @Input()
+  firstH1Right: string = ""
+  @Input()
+  firstPright: string = "";
+  @Input() 
+  secondImageRight: string = "";
+  @Input() 
+  secondH1Right: string = "";
+  @Input()
+  secondPright: string = "";
+  @Input()
+  thirdImageRight: string = "";
+  @Input()
+  thirdH1Right: string = "";
+  @Input()
+  thirdPright: string = "";
+  
+  ngOnInit(): void {}
 
 }
