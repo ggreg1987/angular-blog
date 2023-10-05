00258faf6c7517d111cded3e697ea99ed639f96f
gabriel <gabriel.gregorio1987@gmail.com>
2023-10-05 03:16:12 -0300

    Refactoring the left-card.component.ts

--- src/app/components/left-card/left-card.component.ts
+++ src/app/components/left-card/left-card.component.ts
@@ -13,24 +13,6 @@ export class TextCardComponent implements OnInit {
   textH1: string = "";
   @Input()
   textP: string = "";
-  @Input()
-  firstImageRight: string = ""
-  @Input()
-  firstH1Right: string = ""
-  @Input()
-  firstPright: string = "";
-  @Input() 
-  secondImageRight: string = "";
-  @Input() 
-  secondH1Right: string = "";
-  @Input()
-  secondPright: string = "";
-  @Input()
-  thirdImageRight: string = "";
-  @Input()
-  thirdH1Right: string = "";
-  @Input()
-  thirdPright: string = "";
   
   ngOnInit(): void {}
 
--- src/app/components/right-card/right-card.component.ts
+++ src/app/components/right-card/right-card.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-right-card',
   templateUrl: './right-card.component.html',
   styleUrls: ['./right-card.component.css']
 })
-export class RightCardComponent {
+export class RightCardComponent implements OnInit {
+
+  @Input()
+  firstImageRight: string = ""
+  @Input()
+  firstH1Right: string = ""
+  @Input()
+  firstPright: string = "";
+  @Input() 
+  secondImageRight: string = "";
+  @Input() 
+  secondH1Right: string = "";
+  @Input()
+  secondPright: string = "";
+  @Input()
+  thirdImageRight: string = "";
+  @Input()
+  thirdH1Right: string = "";
+  @Input()
+  thirdPright: string = "";
+
+  ngOnInit(): void {}
 
 }
